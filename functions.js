5d02f3e1628d2557c8c1d7aa8f9e5c898aa09878
varsha <varshalachheta@gmail.com>
2022-10-08 20:39:44 +0530

    Added file

---
+++ functions.js
@@ -0,0 +1,12 @@
+function greet (name , msg){
+    console.log(name  + " From Madhya Pradesh");
+    console.log(msg + " " + name);
+}
+let name = "varsha";
+let name1 = "lata";
+let name2 = "sanju";
+let msg = "good morning";
+// Function calling
+greet (name , msg);
+greet (name1, msg);
+greet (name2, msg);
