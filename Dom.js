83b92dd9c18052ab05f16d846a706ae5c8afb0ec
varsha <varshalachheta@gmail.com>
2022-10-09 16:06:05 +0530

    manipulated dom

---
+++ Dom.js
@@ -0,0 +1,10 @@
+let main = document.getElementById('main');
+console.log(main);
+let nav = document.getElementById('nav');
+console.log(nav);
+let containers = document.getElementsByClassName('container');
+console.log(containers);
+let con = document.querySelector('#nav>li')
+console.log("selector returns", con);
+let all = document.querySelectorAll('#nav>li');
+console.log("selector return", all);
