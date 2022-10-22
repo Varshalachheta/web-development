be670ef4154c67314c1999e96ba38d20039dc32a
varsha <varshalachheta@gmail.com>
2022-10-22 22:05:26 +0530

    math object in javascript

---
+++ mathobj.js
@@ -0,0 +1,17 @@
+let m = Math ;
+console.log(m);
+
+/// Functions
+
+let a = 35.352048;
+let b = 89;
+
+console.log("the value of a and b is ",  a , b);
+console.log("the value of a and b is " , Math.round(a), Math.round(b));
+
+console.log("3 raised to the power of 12 is : " , Math.pow(3,12));
+
+console.log("square root of 36 is : ", Math.sqrt(36));
+
+console.log("5.5 rounded up to nearest integer is ", Math.ceil(5.5));
+console.log("5.5 rounded down to nearest integer is ", Math.floor(5.5));
